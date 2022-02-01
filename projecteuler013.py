e0f939e7de4a42082f58c1f7718805fcf3f84174
ridoineel <ridoineelofficiel@gmail.com>
2022-02-01 22:48:08 +0000

    15 -> 13 fix

---
+++ projecteuler013.py
@@ -0,0 +1,19 @@
+# Work out the first ten digits of the sum of the one-hundred 50-digit numbers in data/data13.txt.
+
+def getData(file_path):
+	file = open(file_path, "r")
+
+	return "".join(file.readlines())
+
+def main():
+	data = getData("data/data13.txt")
+	
+	data = [int(i) for i in data.split()]
+
+	S = sum(data)
+	first_ten = str(S)[:10]
+
+	print(first_ten)
+
+if __name__ == '__main__':
+	main()
