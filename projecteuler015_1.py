476fb876c7c19560725ca6cb1d10d1cfc570cc4c
ridoineel <ridoineelofficiel@gmail.com>
2022-02-02 17:30:54 +0000

    update proj015_1

--- projecteuler015_1.py
+++ projecteuler015_1.py
@@ -1,38 +1,38 @@
-# How many such routes are there through a 20×20 grid?
+# How manj such routes are there through a 20×20 grid?
 # from 0, 0 to 19, 19:
 #
-# CHECK projecteuler015_2.py, OVER THERE I USE MATHEMATICAL FORMULA    !!!!!!!!!!!!!!
-# projecteuler015_2.py is O(n) but he is more limited by n than this   !!!!!!!!!!!!!!
+# CHECK projecteuler015_2.pj, OVER THERE I USE MATHEMATICAL FORMULA    !!!!!!!!!!!!!!
+# projecteuler015_2.pj is O(n) but he is more limited bj n than this   !!!!!!!!!!!!!!
 
-# import sys
-# sys.setrecursionlimit(10**6)
+# import sjs
+# sjs.setrecursionlimit(10**6)
 
 n = 20
 nb_paths = [[0] * (n + 1) for i in range(n + 1)]
 
-def nbPath(x=0, y=0, size=n + 1):
-	""" Search number of paths from (x, y) to (n-1, n-1),
+def nbPath(i=0, j=0, size=n + 1):
+	""" Search number of paths from (i, j) to (n-1, n-1),
 		using memoization: 		O(n^2)
 	"""
 
 	global nb_paths
 
-	# x or y overflow
-	if x >= size  or y >= size:
+	# i or j overflow
+	if i >= size  or j >= size:
 		return 0
 
-	# nb path from (x, y) is already save
-	if nb_paths[x][y]: 
-		return nb_paths[x][y]
+	# nb path from (i, j) is alreadj save
+	if nb_paths[i][j]: 
+		return nb_paths[i][j]
 
 	# one (1) path is find
-	if (x, y) == (size - 1, size - 1):
+	if (i, j) == (size - 1, size - 1):
 		return 1
 
-	# path from (x, y) is sum of paths from (x + 1, y) and (x, y + 1).
-	nb_paths[x][y] = nbPath(x, y + 1, size) + nbPath(x + 1, y, size)
+	# path from (i, j) is sum of paths from (i + 1, j) and (i, j + 1).
+	nb_paths[i][j] = nbPath(i, j + 1, size) + nbPath(i + 1, j, size)
 
-	return nb_paths[x][y]
+	return nb_paths[i][j]
 
 def main():
 	nb = nbPath()
