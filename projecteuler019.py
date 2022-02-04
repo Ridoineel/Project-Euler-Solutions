633a49bbfe9f1725370ecaa314b7d7f35ff5ce6b
ridoineel <ridoineelofficiel@gmail.com>
2022-02-04 01:29:05 +0000

    update proj019

--- projecteuler019.py
+++ projecteuler019.py
@@ -1,9 +1,8 @@
 # How many Sundays fell on the first of the month during 
 # the twentieth century (1 Jan 1901 to 31 Dec 2000)?
 #
-# main2 use datetime
+# main2 use datetime module (with generalization)
 # main use explicit algorithm
-# I generalize this for variable start_year and end_year
 
 from math import ceil
 import datetime
@@ -14,51 +13,45 @@ def main2(): # is not use (call) by default
 	year_end = 2000
 
 	for year in range(year_start, year_end + 1):
-		for mounth in range(1, 13):
-			if datetime.date(year, mounth, 1).weekday() == 6:
+		for month in range(1, 13):
+			if datetime.date(year, month, 1).weekday() == 6:
 				nb_sunday += 1
 
 	print(nb_sunday)
 
 def main():
+	""" Output: number of sundays fell on the first of month 
+		during 1 Jan 1901 -> 31 Dec 2000
+
+	"""
+
 	days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-	nb_sunday = 0
-	year_start = 1901
-	year_end = 2000
+	nb_sundays = 0
+	days_between = 100
+	nb_leap_year = 25
+	total_days = nb_leap_year + days_between*365
 
-	nb_leap = nbLeapYear(year_start, year_end)
-	total_days = nb_leap + (year_end - year_start + 1)*365
-
-	nb_days = 2 # 2 for tuesday (1 jan 1901) (1 -> monday, 3 -> wednesday, ..., 7 -> sunday)
-	mounth_from_start = 1
+	# days_from_start = 1 for monday, 2 -> tuesday 3 -> wednesday, ..., 7 -> sunday
+	days_from_start = 2 # for (1 Jan 1901)
+	month_from_start = 1
 
 	while total_days > 0:
-		if nb_days % 7 == 0:
-			nb_sunday += 1
+		if days_from_start % 7 == 0:
+			nb_sundays += 1
 
-		day_to_add = days[(mounth_from_start - 1) % 12]
+		days_to_add = days[(month_from_start - 1) % 12]
 
-		# if leap year
-		if mounth_from_start % 12 == 2 and ceil(mounth_from_start/12) % 4 == 0:
-			day_to_add += 1
+		# if month is febuary and year is leap year
+		if month_from_start % 12 == 2 and ceil(month_from_start/12) % 4 == 0:
+			days_to_add += 1
 
-		nb_days += day_to_add
-		total_days -= day_to_add
+		days_from_start += days_to_add
+		total_days -= days_to_add
 
-		mounth_from_start += 1
+		month_from_start += 1
 
-	print(nb_sunday)
-
-def nbLeapYear(start, end):
-	if start % 4 != 0:
-		# set start to next divisible by 4 (to get leap lear)
-		start = 4 * (start//4 + 1) 
-
-	nb_leap = 1 + (end - start)/4
-	# 1 because start if now leap year
-
-	return int(nb_leap)
+	print(nb_sundays)
 
 if __name__ == '__main__':
 	main()
